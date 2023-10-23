01bb966d5f90664584d31ab6f8a7f000f4066f30
David Yin <29613693+E137299@users.noreply.github.com>
2023-10-23 21:08:11 +0000

    add one more test to test_scifi_name()

--- test_main.py
+++ test_main.py
@@ -2,3 +2,4 @@ from main import *
 
 def test_scifi_name():
     assert scifi_name("David Yin","Matthews","Austin") == "Yivid Aumat"
+    assert scifi_name("Claire Hoverman", "Casis", "Austin") == "Hoire Aucas"
